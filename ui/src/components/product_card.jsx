a71e109ad2edf6f920592af15971ecadb5b037bc
sjbenenate <sarahjoybenenate@gmail.com>
2024-09-10 22:47:30 -0700

    rating stars

--- ui/src/components/product_card.jsx
+++ ui/src/components/product_card.jsx
@@ -1,18 +1,29 @@
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import { Rating } from './rating_widet';
 
 export const ProductCard = ({ product }) => {
+  const linkUrl = `/product/${product._id}`;
   return (
-    <Link to={`/product/${product._id}`}>
-      <Card className="my-3 p-3">
+    <Card className="my-3 p-3">
+      <Link to={linkUrl}>
         <Card.Img src={product.image}></Card.Img>
-        <Card.Body>
+      </Link>
+      <Card.Body>
+        <Link to={linkUrl}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-          ${product.price}
-        </Card.Body>
-      </Card>
-    </Link>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            msg={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="p">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
