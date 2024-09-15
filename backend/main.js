7a2a327c2d8e0fa67e7492e64df56e59dfe7acfc
sjbenenate <sarahjoybenenate@gmail.com>
2024-09-14 21:58:04 -0700

    basic express route

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,14 @@
-console.log('server main.js running');
+import express from 'express';
+const port = 8800;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server was reached on root route.');
+});
+
+app.get('/new/', (req, res) => {
+  res.send('New route was reached.');
+});
+
+app.listen(port, () => console.log('Server listening on port: ' + port));
