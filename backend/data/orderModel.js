4ae9651a832986a000b4530f99ea851b2acb1590
sjbenenate <sarahjoybenenate@gmail.com>
2024-12-12 18:11:23 -0800

    mogoose order model

--- backend/data/orderModel.js
+++ backend/data/orderModel.js
@@ -1,6 +1,18 @@
 import mongoose from 'mongoose';
 
-const shippingShema = new mongoose.Schema({
+const shippingAddressSchema = new mongoose.Schema({
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: Number, required: true },
+    country: { type: String, required: true },
+    orderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Order',
+    },
+});
+
+const orderItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     qty: { type: Number, required: true },
@@ -19,11 +31,28 @@ const orderSchema = new mongoose.Schema(
             required: true,
             ref: 'User',
         },
-        orderItems: [shippingShema],
+        orderItems: [orderItemSchema],
+        shippingAddressSchema,
+        paymentMethod: {
+            type: String,
+            required: true,
+        },
+        paymentResult: {
+            id: { type: String, required: true },
+            status: { type: String },
+            email: { type: String },
+            updateTime: { type: String },
+        },
+        orderPrice: { type: Number, required: true, default: 0.0 },
+        shippingPrice: { type: Number, required: true, default: 0.0 },
+        totalPrice: { type: Number, required: true, default: 0.0 },
+        isPaid: { type: Boolean, required: true, default: false },
+        isDelivered: { type: Boolean, default: false },
+        deliveredAt: { type: Date },
     },
     {
         timestamps: true,
     }
 );
 
-export default orderSchema = mongoose.model('Order', orderSchema);
+export default orderModel = mongoose.model('Order', orderSchema);
