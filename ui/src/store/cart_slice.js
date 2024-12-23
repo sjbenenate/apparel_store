0ac5d0fa7351a943e9e6896c3a70f6a4e63ce69f
sjbenenate <sarahjoybenenate@gmail.com>
2024-12-22 23:16:09 -0800

    refactor cart slice for id based organization, cart view with specific selector access to cart items for performance

--- ui/src/store/cart_slice.js
+++ ui/src/store/cart_slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
 import { roundDecimals } from '../utils';
 
 const FREE_SHIPPING = 100;
@@ -7,38 +7,56 @@ const STORAGE_KEY = 'cart';
 
 const initialState = localStorage.getItem(STORAGE_KEY)
     ? JSON.parse(localStorage.getItem(STORAGE_KEY))
-    : { cartItems: [] };
+    : { cartItems: {}, cartItemIds: [], prices: {} };
 
+// Edge case handling when localstorage was saved with older version of app
+if (!initialState.cartItemIds) {
+    initialState.cartItemIds = [];
+}
+
+if (!initialState.prices) {
+    initialState.prices = {};
+}
+
+if (!initialState.cartItems) {
+    initialState.cartItems = {};
+}
+
+// reducer helpers
 const updateTotals = (state) => {
-    state.itemsPrice = roundDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    const itemPrices = Object.values(state.cartItems).reduce(
+        (acc, item) => acc + item.price * item.qty,
+        0
     );
-    state.taxPrice = roundDecimals(state.itemsPrice * 0.15);
-    state.shippingPrice = roundDecimals(
-        state.itemsPrice >= FREE_SHIPPING ? 0 : 10
-    );
-    state.totalPrice = roundDecimals(
-        Number(state.itemsPrice) +
-            Number(state.taxPrice) +
-            Number(state.shippingPrice)
-    );
+
+    const tax = itemPrices * 0.15;
+    const shipping = itemPrices >= FREE_SHIPPING ? 0 : 10;
+
+    state.prices = {
+        itemPrices: roundDecimals(itemPrices),
+        tax: roundDecimals(tax),
+        shipping: roundDecimals(shipping),
+        total: roundDecimals(itemPrices + tax + shipping),
+    };
 };
 
+// The Slice
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
         addItemToCart: (state, action) => {
             const item = action.payload;
-            const itemExists = state.cartItems.some((i) => i._id === item._id);
+            const itemId = action.payload._id;
+            const itemExists = state.cartItems[itemId];
+
             if (!itemExists) {
-                state.cartItems = [...state.cartItems, item];
+                state.cartItems = { ...state.cartItems, [itemId]: item };
+                state.cartItemIds = [...state.cartItemIds, itemId];
             } else {
-                state.cartItems = state.cartItems.map((i) =>
-                    i._id === item._id ? { ...item, qty: i.qty + item.qty } : i
-                );
+                state.cartItems[itemId].qty += item.qty;
             }
-            updateTotals(state);
+            updateTotals(state, itemId);
             try {
                 localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
             } catch (err) {
@@ -48,7 +66,24 @@ const cartSlice = createSlice({
     },
 });
 
+// Actions and Reducer exports
 export const { addItemToCart } = cartSlice.actions;
 export const CartSliceReducers = cartSlice.reducer;
-export const selectCartQty = (state) =>
-    state.cart.cartItems.reduce((acc, item) => acc + item.qty, 0);
+
+// Selectors
+export const selectCartItemIds = (state) => state.cart.cartItemIds;
+
+export const selectCartPrices = (state) => state.cart.prices;
+
+export const selectCartQty = (state) => {
+    return state.cart.cartItemIds.reduce(
+        (acc, id) => acc + state.cart.cartItems[id]?.qty,
+        0
+    );
+};
+export const selectCart = (state) => state.cart;
+
+export const createSelectCartItem = (itemId) =>
+    createSelector([selectCart], (cart) =>
+        cart.cartItems[itemId] ? cart.cartItems[itemId] : {}
+    );
--- ui/src/views/cart_view.jsx
+++ ui/src/views/cart_view.jsx
@@ -1,9 +1,43 @@
+import { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Row, Col, Form, Button, Card } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import {
+    selectCart,
+    selectCartPrices,
+    createSelectCartItem,
+    selectCartItemIds,
+} from '../store/cart_slice';
+
+const CartRow = ({ itemId }) => {
+    /*const selectItem = useMemo(
+        (itemId) => createSelectCartItem(itemId),
+        [itemId]
+    );
+    const cartItem = useSelector(selectItem);*/
+
+    const selectItem = createSelectCartItem(itemId);
+    const cartItem = useSelector(selectItem);
+
+    console.log(cartItem);
+
+    return <Row>{itemId}</Row>;
+};
 
 const CartView = () => {
-    return <div>Cart</div>;
+    const cartItemIds = useSelector(selectCartItemIds);
+
+    const getCartRows = () => {
+        if (cartItemIds.length < 1) return 'No items in cart';
+        return cartItemIds.map((id) => <CartRow key={id} itemId={id} />);
+    };
+
+    return (
+        <div>
+            <h1>Cart</h1>
+            <Col>{getCartRows()}</Col>
+        </div>
+    );
 };
 
 export default CartView;
