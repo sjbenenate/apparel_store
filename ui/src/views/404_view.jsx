d7f86979f3b94e8c143468da053d9887e4ae0832
sjbenenate <sarahjoybenenate@gmail.com>
2024-12-22 19:14:02 -0800

    header and footer added to 404 page

--- ui/src/views/404_view.jsx
+++ ui/src/views/404_view.jsx
@@ -1,15 +1,21 @@
 import { useRouteError } from 'react-router-dom';
+import { Header } from '../components/header';
+import { Footer } from '../components/footer';
 
 export const NotFoundView = () => {
     const routeError = useRouteError();
     console.error(routeError);
 
     return (
-        <div className="text-center">
-            <h2 className="display-1">Status 404</h2>
-            <p className="lead">
-                The page you are looking for could not be found!
-            </p>
+        <div>
+            <Header />
+            <div className="text-center">
+                <h2 className="display-1">Status 404</h2>
+                <p className="lead">
+                    The page you are looking for could not be found!
+                </p>
+            </div>
+            <Footer />
         </div>
     );
 };
