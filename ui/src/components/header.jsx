7792e7085b7b22328f61734eddc40ec0195bd474
sjbenenate <sarahjoybenenate@gmail.com>
2024-12-22 19:11:03 -0800

    better icon for cart qty

--- ui/src/components/header.jsx
+++ ui/src/components/header.jsx
@@ -2,15 +2,25 @@ import { LinkContainer } from 'react-router-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
 import { GiShoppingCart } from 'react-icons/gi';
 import { CiUser } from 'react-icons/ci';
 import logo from '../img/peacock_logo.png';
 import { useSelector } from 'react-redux';
 import { selectCartQty } from '../store/cart_slice';
 
-export const Header = () => {
-    const qtyItems = useSelector(selectCartQty);
+const CartQtyIcon = () => {
+    const cartQty = useSelector(selectCartQty);
 
+    if (!cartQty) return;
+    return (
+        <Badge pill bg="info" style={{ marginLeft: '5px' }}>
+            {cartQty}
+        </Badge>
+    );
+};
+
+export const Header = () => {
     return (
         <Navbar expand="md" data-bs-theme="dark" bg="dark" collapseOnSelect>
             <Container>
@@ -36,7 +46,7 @@ export const Header = () => {
                         <LinkContainer to="/cart">
                             <Nav.Link>
                                 <GiShoppingCart /> Cart
-                                {qtyItems ? ` (${qtyItems})` : null}
+                                <CartQtyIcon />
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/login">
