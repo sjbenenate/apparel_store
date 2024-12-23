ad95918d6e5c8cf0e6af630ef0ab7c1278d78ac2
sjbenenate <sarahjoybenenate@gmail.com>
2024-12-23 00:08:10 -0800

    component for qty select dropdown

--- ui/src/components/controls.jsx
+++ ui/src/components/controls.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 
 const RouteButton = ({ route = '/', text = 'Back' }) => {
     return (
@@ -8,4 +9,24 @@ const RouteButton = ({ route = '/', text = 'Back' }) => {
     );
 };
 
-export { RouteButton };
+const QtySelect = ({ currentQty, qtyInStock, onChange }) => {
+    if (!qtyInStock) return;
+    return (
+        <Form.Select
+            value={currentQty}
+            disabled={!qtyInStock}
+            onChange={onChange}
+        >
+            {[...Array(qtyInStock).keys()].map((i) => {
+                const v = i + 1;
+                return (
+                    <option key={v} value={v}>
+                        {v}
+                    </option>
+                );
+            })}
+        </Form.Select>
+    );
+};
+
+export { RouteButton, QtySelect };
--- ui/src/views/product_view.jsx
+++ ui/src/views/product_view.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import { Form, Button, Col, Row, Image, ListGroup } from 'react-bootstrap';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Button, Col, Row, Image, ListGroup } from 'react-bootstrap';
 import { Rating } from '../components/rating_widget';
 import { useGetProductInfoQuery } from '../store/api_products';
 import Loader from '../components/loader.jsx';
 import Message from '../components/message.jsx';
 import { useDispatch } from 'react-redux';
 import { addItemToCart } from '../store/cart_slice.js';
-import { RouteButton } from '../components/controls.jsx';
+import { RouteButton, QtySelect } from '../components/controls.jsx';
 
 export const ProductView = () => {
     const { productId } = useParams();
@@ -26,29 +26,6 @@ export const ProductView = () => {
         console.log(`adding to cart`);
         dispatch(addItemToCart({ ...product, qty: Number(qty) }));
         navigate('/cart');
-    };
-
-    const qtySelect = (qtyInStock) => {
-        if (!qtyInStock) return;
-        return (
-            <Form.Select
-                value={qty}
-                disabled={!qtyInStock}
-                onChange={(e) => {
-                    console.log(`setting qty to ${e.target.value}`);
-                    setQty(e.target.value);
-                }}
-            >
-                {[...Array(qtyInStock).keys()].map((i) => {
-                    const v = i + 1;
-                    return (
-                        <option key={v} value={v}>
-                            {v}
-                        </option>
-                    );
-                })}
-            </Form.Select>
-        );
     };
 
     const productInfo = (product) => {
@@ -89,7 +66,11 @@ export const ProductView = () => {
                             </p>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            {qtySelect(product.countInStock)}
+                            <QtySelect
+                                currentQty={qty}
+                                qtyInStock={product.countInStock}
+                                onChange={(e) => setQty(e.target.value)}
+                            />
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <Button
