1d2af6872652a1db158cdf2553231e059b5f3258
sjbenenate <sarahjoybenenate@gmail.com>
2025-01-06 22:46:00 -0800

    fixed bug with qty added from product page

--- ui/src/views/product_view.jsx
+++ ui/src/views/product_view.jsx
@@ -24,7 +24,7 @@ export const ProductView = () => {
 
     const addToCart = () => {
         console.log(`adding to cart`);
-        dispatch(addItemToCart({ ...product, qty }));
+        dispatch(addItemToCart({ ...product, qty: Number(qty) }));
         navigate('/cart');
     };
 
