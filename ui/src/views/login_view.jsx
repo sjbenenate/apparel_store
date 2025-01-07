13eaeea1cb963f851fea32490aa1974354205350
sjbenenate <sarahjoybenenate@gmail.com>
2025-01-06 23:09:28 -0800

    register link on login page

--- ui/src/views/login_view.jsx
+++ ui/src/views/login_view.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container, Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import FormContainer from '../components/form_container';
-import { login } from '../store/auth_slice';
+import { login } from '../store/api_users';
 
 const LoginView = () => {
     const [inputEmail, setInputEmail] = useState('');
@@ -34,9 +35,17 @@ const LoginView = () => {
                             onChange={(e) => setInputPassword(e.target.value)}
                         />
                     </Form.Group>
-                    <Button type="submit" variant="info">
-                        Submit
-                    </Button>
+                    <Form.Group className="d-flex flex-wrap justify-content-between">
+                        <Button type="submit" variant="info">
+                            Submit
+                        </Button>
+                        <div className="d-inline-block pt-2">
+                            New Customer?{' '}
+                            <span>
+                                <Link to="/register">Register</Link>
+                            </span>
+                        </div>
+                    </Form.Group>
                 </Form>
             </FormContainer>
         </Container>
