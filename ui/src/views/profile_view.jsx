0303eecc7c0dc23abaf33238881883057e64a215
sjbenenate <sarahjoybenenate@gmail.com>
2025-01-16 10:54:30 -0800

    profile view display name and email

--- ui/src/views/profile_view.jsx
+++ ui/src/views/profile_view.jsx
@@ -1,11 +1,30 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
 import FormContainer from '../components/form_container';
+import { useSelector } from 'react-redux';
+import { selectAuthInfo } from '../store/auth_slice';
+
+const UserInfoItem = ({ label, value }) => {
+    return (
+        <ListGroup.Item>
+            <Row>
+                <Col>{`${label}: `}</Col>
+                <Col>{value}</Col>
+            </Row>
+        </ListGroup.Item>
+    );
+};
 
 const ProfileView = () => {
+    const userInfo = useSelector(selectAuthInfo);
+
     return (
         <Container>
             <FormContainer>
                 <h1>Profile</h1>
+                <ListGroup>
+                    <UserInfoItem label="Name" value={userInfo?.name} />
+                    <UserInfoItem label="Email" value={userInfo?.email} />
+                </ListGroup>
             </FormContainer>
         </Container>
     );
