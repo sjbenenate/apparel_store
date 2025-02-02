0f1ea907d5eab2cfdea826b8ccdd9d6cd2199ba0
sjbenenate <sarahjoybenenate@gmail.com>
2025-02-01 19:40:37 -0800

    orders history cleaning up

--- ui/src/components/orders_history.jsx
+++ ui/src/components/orders_history.jsx
@@ -1,6 +1,7 @@
 import { Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { useGetUserOrdersQuery } from '../store/api_orders';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaTimes } from 'react-icons/fa';
 
 const OrdersHistory = () => {
     const ordersQuery = useGetUserOrdersQuery();
@@ -11,40 +12,44 @@ const OrdersHistory = () => {
         : [];
 
     return (
-        <Table>
-            <thead>
+        <Table striped responsive hover className="table-sm">
+            <thead align="center">
                 <tr>
+                    <td>Date</td>
                     <td>Order ID</td>
                     <td>Items</td>
                     <td>Paid</td>
-                    <td>Date</td>
+                    <td>Delivered</td>
                 </tr>
             </thead>
-            <tbody>
+            <tbody align="center">
                 {orders.map((order, index) => {
                     return (
-                        <tr key={index}>
-                            <td>
-                                <Link to={`/order/${order._id}`}>
-                                    {order._id}
-                                </Link>
-                            </td>
-                            <td>
-                                <strong>Items: </strong>
-                                {order.orderItems.length}
-                            </td>
-                            <td
-                                style={{
-                                    background: order.isPaid ? 'green' : 'red',
-                                }}
-                            >
-                                {order.isPaid ? 'Yes' : 'No'}
-                            </td>
-                            <td>
-                                <strong>Date: </strong>
-                                {new Date(order.createdAt).toLocaleDateString()}
-                            </td>
-                        </tr>
+                        <LinkContainer to={`/order/${order._id}`} key={index}>
+                            <tr>
+                                <td>
+                                    {new Date(
+                                        order.createdAt
+                                    ).toLocaleDateString()}
+                                </td>
+                                <td>{order._id}</td>
+                                <td>{order.orderItems.length}</td>
+                                <td
+                                    style={{
+                                        background: order.isPaid
+                                            ? 'green'
+                                            : 'red',
+                                    }}
+                                >
+                                    {order.isPaid ? 'Yes' : 'No'}
+                                </td>
+                                <td>
+                                    {new Date(
+                                        order.createdAt
+                                    ).toLocaleDateString()}
+                                </td>
+                            </tr>
+                        </LinkContainer>
                     );
                 })}
             </tbody>
