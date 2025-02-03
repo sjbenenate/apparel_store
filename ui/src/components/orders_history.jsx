b1d142d68f07a1c605beedcbb85f2d36e474e496
sjbenenate <sarahjoybenenate@gmail.com>
2025-02-02 21:05:06 -0800

    order table icon for unpaid and undelivered

--- ui/src/components/orders_history.jsx
+++ ui/src/components/orders_history.jsx
@@ -1,6 +1,7 @@
 import { Table } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FaTimes } from 'react-icons/fa';
+import { localTimeString } from '../utils';
 
 const OrderRow = ({ order }) => {
     return (
@@ -9,14 +10,20 @@ const OrderRow = ({ order }) => {
                 <td>{new Date(order.createdAt).toLocaleDateString()}</td>
                 <td>{order._id}</td>
                 <td>{order?.orderItems?.length}</td>
-                <td
-                    style={{
-                        background: order.isPaid ? 'green' : 'red',
-                    }}
-                >
-                    {order.isPaid ? 'Yes' : 'No'}
+                <td>
+                    {order.isPaid ? (
+                        localTimeString(order.paidAt)
+                    ) : (
+                        <FaTimes color="red" />
+                    )}
                 </td>
-                <td>{new Date(order.createdAt).toLocaleDateString()}</td>
+                <td>
+                    {order.isDelivered ? (
+                        localTimeString(order.deliveredAt)
+                    ) : (
+                        <FaTimes color="red" />
+                    )}
+                </td>
             </tr>
         </LinkContainer>
     );
--- ui/src/utils.js
+++ ui/src/utils.js
@@ -2,4 +2,8 @@ const roundDecimals = (value) => {
     return (Math.round(value * 100) / 100).toFixed(2);
 };
 
-export { roundDecimals };
+const localTimeString = (datetime) => {
+    return new Date(datetime).toLocaleDateString();
+};
+
+export { roundDecimals, localTimeString };
