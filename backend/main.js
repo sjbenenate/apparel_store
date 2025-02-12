59f1c601e6ea3b8aec0ad10703c76d37260425f5
sjbenenate <sarahjoybenenate@gmail.com>
2025-02-11 19:33:09 -0800

    fixed static folder declaration on sever

--- backend/main.js
+++ backend/main.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 app.use(cookieParser());
 
 // Static storage
-app.use(express.static(path.resolve('./uploads')));
+app.use('/uploads', express.static(path.resolve('./uploads')));
 
 // Routes
 app.use('/api/products', productRouter);
